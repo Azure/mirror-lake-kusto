e53148db177982e1f95bb980b8e3401d73d94d70
vplauzon <vincent_lauzon@yahoo.com.au>
2022-07-09 01:08:22 -0400

    Add test project

---
+++ code/KustoMirrorTest/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace KustoMirrorTest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            //  https://github.com/Azure/azure-sdk-for-net/blob/Azure.Analytics.Synapse.Spark_1.0.0-preview.8/sdk/synapse/Azure.Analytics.Synapse.Spark/samples/Sample2_ExecuteSparkStatementAsync.md
+        }
+    }
+}
