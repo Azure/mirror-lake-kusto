e1060808df6cbd547d1b3d547389d438de5cec11
vplauzon <vincent_lauzon@yahoo.com.au>
2022-07-18 12:54:01 -0400

    .

---
+++ code/KustoMirrorTest/SparkSingleton.cs
@@ -0,0 +1,33 @@
+﻿using Azure.Analytics.Synapse.Spark.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KustoMirrorTest
+{
+    internal class SparkSingleton
+    {
+        #region Constructors
+        static SparkSingleton()
+        {
+            Instance = new SparkSingleton();
+        }
+
+        private SparkSingleton()
+        {
+        }
+        #endregion
+
+        public static SparkSingleton Instance { get; }
+
+        public async Task<SparkSession> GetSparkSessionAsync()
+        {
+            //var sparkPoolName = "quicktests";
+            await Task.CompletedTask;
+
+            throw new NotImplementedException();
+        }
+    }
+}
