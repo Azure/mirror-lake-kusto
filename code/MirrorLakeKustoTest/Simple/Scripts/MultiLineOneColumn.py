318615f0be98731503824fbf02955a95fe5abc3a
vplauzon <vincent_lauzon@yahoo.com.au>
2022-10-02 16:30:58 -0400

    Add multi-line test

---
+++ code/MirrorLakeKustoTest/Simple/Scripts/MultiLineOneColumn.py
@@ -0,0 +1,5 @@
+﻿#	Create a range of 10 rows with one column
+data = spark.range(0,10)
+
+#	Persist the range as a delta table
+data.write.format("delta").save("<ROOT>/delta")
--- code/MirrorLakeKustoTest/Simple/Simple.cs
+++ code/MirrorLakeKustoTest/Simple/Simple.cs
@@ -20,5 +20,27 @@ namespace MirrorLakeKustoTest.Simple
                 Assert.Equal(0, ids.FirstOrDefault());
             }
         }
+
+        [Fact]
+        public async Task MultiLineOneColumn()
+        {
+            await using (var session = await GetTestSessionAsync("delta", "MultiLine"))
+            {
+                var script = session.GetResource("MultiLineOneColumn.py");
+                var output = await session.ExecuteSparkCodeAsync(script);
+
+                await session.RunMirrorAsync();
+
+                var ids = await session.ExecuteQueryAsync(
+                    "",
+                    r => (long)r["id"]);
+
+                Assert.Equal(10, ids.Count);
+                for (int i = 0; i != 10; ++i)
+                {
+                    Assert.Contains(ids, i);
+                }
+            }
+        }
     }
 }
