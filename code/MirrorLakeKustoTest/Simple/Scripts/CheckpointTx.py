7bcbb9a9f344906445bc45e72094f69c8a40fd9e
vplauzon <vincent_lauzon@yahoo.com.au>
2022-10-02 17:01:42 -0400

    .

--- code/MirrorLakeKustoTest/Simple/Scripts/CheckpointTx.py
+++ code/MirrorLakeKustoTest/Simple/Scripts/CheckpointTx.py
@@ -5,6 +5,6 @@ data = spark.range(0,1)
 data.write.format("delta").save("<ROOT>/delta")
 
 #   Create ten more transactions
-for i in range(1,10):
+for i in range(1,11):
     data = spark.range(i,i+1)
     data.write.format("delta").mode("append").save("<ROOT>/delta")
