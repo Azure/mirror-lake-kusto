46a95f7afebc9bba3017fab74984eab5fe72ef38
vplauzon <vincent_lauzon@yahoo.com.au>
2022-10-03 15:43:25 -0400

    Add a test with partition data

--- code/MirrorLakeKustoTest/Electric/ElectricTest.cs
+++ code/MirrorLakeKustoTest/Electric/ElectricTest.cs
@@ -25,5 +25,23 @@ namespace MirrorLakeKustoTest.Electric
                 Assert.Equal(467855, rowCounts.First());
             }
         }
+
+        [Fact]
+        public async Task PartitionLoad()
+        {
+            await using (var session = await GetTestSessionAsync("delta", "Electric"))
+            {
+                var script = session.GetResource("PartitionLoad.py");
+                var output = await session.ExecuteSparkCodeAsync(script);
+
+                await session.RunMirrorAsync();
+
+                var rowCounts = await session.ExecuteQueryAsync(
+                    "| where ModelYear==1994 | count",
+                    r => (long)r[0]);
+
+                Assert.Equal(3, rowCounts.First());
+            }
+        }
     }
 }
---
+++ code/MirrorLakeKustoTest/Electric/Scripts/PartitionLoad.py
@@ -0,0 +1,17 @@
+﻿from pyspark.sql.functions import *
+
+#	Load data set
+vehicles = spark.read.option("header",True).option("inferSchema",True).csv('<TEST_DATA>/Electric_Vehicle_Title_and_Registration_Activity.csv.gz')
+
+#   Alias columns to have legal column names
+vehiclesClean=vehicles.select(
+    col(vehicles.columns[0]).alias('VehicleType'),
+    col(vehicles.columns[1]).alias('Vin'),
+    col(vehicles.columns[2]).alias('ModelYear'),
+    col(vehicles.columns[3]).alias('Make'),
+    col(vehicles.columns[4]).alias('Model'),
+    col(vehicles.columns[5]).alias('NewUsed'))
+
+
+#	Persist the range as a delta table
+vehiclesClean.write.partitionBy("ModelYear").format("delta").save("<ROOT>/delta")
