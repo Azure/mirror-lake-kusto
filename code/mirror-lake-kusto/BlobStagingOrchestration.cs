4ca565b6ba71994a349538ae155c1bb24c9e9b6b
vplauzon <vincent_lauzon@yahoo.com.au>
2022-10-06 14:48:28 -0400

    Pipeline width

--- code/mirror-lake-kusto/BlobStagingOrchestration.cs
+++ code/mirror-lake-kusto/BlobStagingOrchestration.cs
@@ -1,5 +1,6 @@
 ﻿using MirrorLakeKusto.Kusto;
 using MirrorLakeKusto.Storage;
+using System.Collections.Concurrent;
 
 namespace MirrorLakeKusto
 {
@@ -8,7 +9,7 @@ namespace MirrorLakeKusto
         private readonly DatabaseGateway _databaseGateway;
         private readonly TableDefinition _stagingTable;
         private readonly TableStatus _tableStatus;
-        private readonly long _startTxId;
+        private readonly ConcurrentQueue<IEnumerable<TransactionItem>> _itemsBatchToIngest;
 
         #region Constructors
         public static async Task EnsureAllStagedAsync(
@@ -33,19 +34,39 @@ namespace MirrorLakeKusto
             TableStatus tableStatus,
             long startTxId)
         {
+            var logs = tableStatus.GetBatch(startTxId);
+            var itemsToIngest = logs.Adds;
+
             _databaseGateway = databaseGateway;
             _stagingTable = stagingTable;
             _tableStatus = tableStatus;
-            _startTxId = startTxId;
+            _itemsBatchToIngest = new ConcurrentQueue<IEnumerable<TransactionItem>>(new[]
+                {
+                    itemsToIngest
+                });
         }
         #endregion
 
         #region Orchestration
         private async Task RunAsync(CancellationToken ct)
         {
-            await Task.CompletedTask;
+            var pipelineWidth = await ComputePipelineWidthAsync(ct);
 
             throw new NotImplementedException();
+        }
+
+        private async Task<int> ComputePipelineWidthAsync(CancellationToken ct)
+        {
+            var ingestionSlots = await _databaseGateway.ExecuteCommandAsync(
+                @".show capacity
+| where Resource == ""Ingestions""
+| project Total",
+                r => (long)r[0],
+                ct);
+            var ingestionSlotCount = (int)ingestionSlots.First();
+            var width = Math.Min(ingestionSlotCount, _itemsBatchToIngest.Count);
+
+            return width;
         }
         #endregion
     }
